f80630b05519487e6fa7315787fe225120088d6e
Junior <junior@salestracker.com>
2026-08-15 18:36:54 -0400

    fix: ruta raiz sirve index.html

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $path = base_path('public/index.html');
-    if (!file_exists($path)) {
-        $path = public_path('index.html');
-    }
-    if (file_exists($path)) {
-        return response(file_get_contents($path), 200)
-            ->header('Content-Type', 'text/html')
-            ->header('Cache-Control', 'no-cache');
-    }
-    return response('Sales Tracker - Frontend not built', 500);
+    $content = file_get_contents(public_path('index.html'));
+    return response($content, 200)
+        ->header('Content-Type', 'text/html')
+        ->header('Cache-Control', 'no-cache');
 });
