a3058b200cdddad33e4d8697f2944347210f2914
Junior <junior@salestracker.com>
2026-08-15 18:25:09 -0400

    fix: servir React build en ruta raiz ( RouteServiceProvider)

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/{any?}', function () {
+    return response()->file(public_path('index.html'));
+})->where('any', '.*');
