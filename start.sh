098daa7cb64a6afb69349f55bfeab91dda723647
Junior <junior@salestracker.com>
2026-08-15 18:58:38 -0400

    fix: start.sh ejecuta nginx+php-fpm en vez de artisan serve

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-/usr/local/bin/php artisan migrate --force
-/usr/local/bin/php artisan db:seed --force
-/usr/local/bin/php artisan serve --host=0.0.0.0 --port=$PORT
+php-fpm -D
+nginx -g "daemon off;"
